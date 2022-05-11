a4065ce31715a28b1a939775da6fc9b0839ab30d
Elber Rondón <78270677+rondon18@users.noreply.github.com>
2022-05-11 07:32:28 -0430

    Update ayuda.php

--- ayuda.php
+++ ayuda.php
@@ -87,7 +87,7 @@
 						</div>
 				  </div>
 				</div>
-        <a href="/proyecto_pst/Manual.php" target="_blank"><i class="fas fa-download fa-lg me-2"></i>Descargar manual de usuario</a>
+        <a href="/proyecto_pst/docs/Manual de usuario.pdf" target="_blank"><i class="fas fa-download fa-lg me-2"></i>Descargar manual de usuario</a>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
