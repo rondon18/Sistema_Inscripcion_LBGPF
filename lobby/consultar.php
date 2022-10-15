54f3a14eb4ba6263c1cd41a78f9ce24f64327d34
AnniePans <103113808+AnniePans@users.noreply.github.com>
2022-10-14 21:29:12 -0400

    Cambio consultar y nuevo respaldo bd

--- lobby/consultar.php
+++ lobby/consultar.php
@@ -430,7 +430,7 @@ if ($_SESSION['usuario']['Privilegios'] == 1) {
 				{
           extend: 'excelHtml5',
 					exportOptions: {
-            columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+            columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
         	},                	                       
           text: 'Generar reporte en Excel <i class="fa-solid fa-file-excel fa-lg ms-2"></i>',
           autoFilter: true,
