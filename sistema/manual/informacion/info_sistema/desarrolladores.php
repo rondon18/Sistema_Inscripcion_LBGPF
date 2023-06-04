7101f08fdc22cc5ebb33f12ac08e7e5e242f9b24
Gab952 <101819115+Gab952@users.noreply.github.com>
2023-06-04 14:19:56 -0400

    actualizació de la ayuda

--- sistema/manual/informacion/info_sistema/desarrolladores.php
+++ sistema/manual/informacion/info_sistema/desarrolladores.php
@@ -3,30 +3,35 @@
 <section class="row row-cols-1">
 
 	
-	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius obcaecati repellendus ut libero vero possimus soluta voluptatibus aspernatur laudantium qui repudiandae, quidem perspiciatis quo odio reprehenderit debitis provident magnam maiores.
-	</p>
+<h1>Acerca de los desarrolladores</h1>	
 
-	<dl class="row">
-		<dt class="col-sm-3">User Agent</dt>
-		<dd class="col-sm-9">An HTML user agent is any device that interprets HTML documents.</dd>
-		
-		<dt class="col-sm-3 text-truncate">Client-side Scripting</dt>
-		<dd class="col-sm-9">
-			Client-side scripting generally refers to the category of computer programs on the web that are executed by the user's web browser.
-		</dd>
+<p>Información general de los desarrolladores del sistema</p>
 
-		<dt class="col-sm-3">Document Tree</dt>
-		<dd class="col-sm-9">The tree of elements encoded in the source document.</dd>
-	</dl>
+<div class="px-3">
+  
+  <p>
+    Los autores de este software de inscripción son un equipo de estudiantes universitarios compuesto por:
+  </p>
+
+  <p>
+  	Adrián Ganzález: Técnico Superior Universitario en Informática.
+	Correo: adrian2001.gm@gmail.com
+  </p>
+
+  <p>
+	Elber Rondón: Técnico Superior Universitario en Informática.
+	Correo: elber.alonso16@gmail.com
+  </p>
+
+  <p>
+	María Ballestero: Técnico Superior Universitario en Informática.
+	Correo: mgbrodriguez952@gmail.com
+  </p>
+
+  <p>
+  	Estos jóvenes desarrolladores tienen experiencia en el campo del desarrollo de software utilizando herramientas como PHP, HTML, JavaScript y CSS; y han creado un programa de inscripción altamente efectivo y fácil de usar. Si necesitas contactarlos, puedes utilizar la información proporcionada anteriormente.
+  </p>
+
+</div>
 
 </section>
-Los autores de este software de inscripción son un equipo de estudiantes universitarios compuesto por:
-
-    Juan Pérez: juan.perez@desarrollador.com
-
-    Ana García: cuenta de Twitter @anagsistemas
-
-    Luis Gómez: luis.gomez@desarrollador.com
-
-Estos jóvenes desarrolladores tienen experiencia en el campo del desarrollo de software y han creado un programa de inscripción altamente efectivo y fácil de usar. Si necesitas contactarlos, puedes utilizar la información proporcionada anteriormente.
