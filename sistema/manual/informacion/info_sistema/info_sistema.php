6dfea26099bee779fa2f30bf2044f5c4a97bb682
Gab952 <101819115+Gab952@users.noreply.github.com>
2023-06-04 13:09:47 -0400

    actualización de ayuda

--- sistema/manual/informacion/info_sistema/info_sistema.php
+++ sistema/manual/informacion/info_sistema/info_sistema.php
@@ -3,21 +3,51 @@
 <section class="row row-cols-1">
 
 	
-	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius obcaecati repellendus ut libero vero possimus soluta voluptatibus aspernatur laudantium qui repudiandae, quidem perspiciatis quo odio reprehenderit debitis provident magnam maiores.
-	</p>
+<h1>Acerca del sistema</h1>	
 
-	<dl class="row">
-		<dt class="col-sm-3">User Agent</dt>
-		<dd class="col-sm-9">An HTML user agent is any device that interprets HTML documents.</dd>
-		
-		<dt class="col-sm-3 text-truncate">Client-side Scripting</dt>
-		<dd class="col-sm-9">
-			Client-side scripting generally refers to the category of computer programs on the web that are executed by the user's web browser.
-		</dd>
+<p>Información general del sistema</p>
 
-		<dt class="col-sm-3">Document Tree</dt>
-		<dd class="col-sm-9">The tree of elements encoded in the source document.</dd>
-	</dl>
+<div class="px-3">
+  
+  <p>
+  	El sistema de inscripción de estudiantes está diseñado para permitir un flujo de trabajo más eficiente así como de mantener la información almacenada de mejor manera. Por lo tanto cuenta con una serie de funciones que permiten que estos objetivos como: registrar a los estudiantes con todos sus datos pertinentes junto con los datos de su representante y padres; consultar los datos de los estudiantes ya inscritos, general reportes generales de los estudiantes, padres y representantes y las planillas de inscripción correspondientes; asimismo, se puede consultar las estadísticas de los datos almacenados, cabe destacar que los reportes generales, las consultas y las estadísticas cuentan con un filtro que les permite a los usuarios escoger la información que necesitan en el momento; también el sistema permite al administrador generar respaldos de la base de datos así como restaurarla de ser necesario.
+  </p>
+
+  <p>
+  	El mantenimiento del sistema será realizado por los técnicos competentes y se manejará de acuerdo a la necesidad del mismo.
+  </p>
+
+  <p>
+  	El sistema cumple con los estándares de seguridad y, a su vez, los usuarios deben cumplir con las políticas de seguridad establecidas por los desarrolladores.
+  </p>
+
+</div>
+
+<h1>Información del sistema</h1>	
+<p>Detalles del sistema</p>
+
+<div class="px-3">
+  
+  <p>
+  	Versión: 2.1
+  </p>
+
+  <p>
+  	Actualización: 6  jun 2023.
+  </p>
+
+  <p>
+	Tamaño del sistema: 47.9 MB
+  </p>
+
+  <p>
+  	SO requerido: Windows 7 y versiones posteriores, 64 bits.
+  </p>
+
+  <p>
+  	Fecha de lanzamiento: 14 sep 2022.
+  </p>
+
+</div>
 
 </section>
