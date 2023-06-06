249aa27c65602fb730c7ca64ae0d8f6ebb3ff0a5
Elber Rondón <78270677+rondon18@users.noreply.github.com>
2023-06-06 10:19:39 -0400

    Reversión de los cambios en la clase personas

--- sistema/clases/personas.php
+++ sistema/clases/personas.php
@@ -66,7 +66,6 @@
 				`cedula` = `cedula`;
 			";
 
-			// echo $sql;
 
 			$conexion->query($sql) or die("error: ".$conexion->error);
 
@@ -90,24 +89,23 @@
 
 			$sql = "
 				UPDATE
-					`personas`
+			    `personas`
 				SET
 					`cedula` = '$cedula',
-					`p_nombre` = '$p_nombre',
-					`s_nombre` = '$s_nombre',
-					`p_apellido` = '$p_apellido',
-					`s_apellido` = '$s_apellido',
-					`fecha_nacimiento` = '$fecha_nacimiento',
-					`lugar_nacimiento` = '$lugar_nacimiento',
-					`genero` = '$genero',
-					`estado_civil` = '$estado_civil',
-					`email` = '$email',
-					`grado_academico` = '$grado_academico'
+			    `p_nombre` = '$p_nombre',
+			    `s_nombre` = '$s_nombre',
+			    `p_apellido` = '$p_apellido',
+			    `s_apellido` = '$s_apellido',
+			    `fecha_nacimiento` = '$fecha_nacimiento',
+			    `lugar_nacimiento` = '$lugar_nacimiento',
+			    `genero` = '$genero',
+			    `estado_civil` = '$estado_civil',
+			    `email` = '$email',
+			    `grado_academico` = '$grado_academico'
 				WHERE
 					`cedula` = '$cedula_actual';
 			";
 
-			// echo $sql;
 
 			$conexion->query($sql) or die("error: ".$conexion->error);
 
@@ -127,7 +125,6 @@
 				`cedula` = '$cedula';
 			";
 
-			// echo $sql;
 
 			$conexion->query($sql) or die("error: ".$conexion->error);
 
@@ -142,8 +139,6 @@
 				FROM
 					`personas`
 			";
-
-			// echo $sql;
 			
 			$resultado = $conexion->query($sql) or die("error: ".$conexion->error);
 
@@ -202,48 +197,23 @@
 
 		// SETTERS
 		public function set_cedula($cedula) {
-			if (ctype_alnum($cedula) and (strlen($cedula) >= 3 && strlen($cedula) <= 15)) {
-				$this->cedula = $cedula;
-			}
-			else {
-				throw new Exception("Se ingresó un valor no alfanumérico");
-			}
+			$this->cedula = $cedula;
 		}
 		
 		public function set_p_nombre($p_nombre) {
-			if (ctype_alpha($p_nombre) and (strlen($p_nombre) >= 3 && strlen($p_nombre) <= 25)) {
-				$this->p_nombre = $p_nombre;
-			}
-			else {
-				throw new Exception("Se ingresó un valor no alfabético");
-			}
+			$this->p_nombre = $p_nombre;
 		}
 		
 		public function set_s_nombre($s_nombre) {
-			if (ctype_alpha($s_nombre) and (strlen($s_nombre) <= 25)) {
-				$this->s_nombre = $s_nombre;
-			}
-			else {
-				throw new Exception("Se ingresó un valor no alfabético");
-			}
+			$this->s_nombre = $s_nombre;
 		}
 		
 		public function set_p_apellido($p_apellido) {
-			if (ctype_alpha($p_apellido) and (strlen($p_apellido) >= 3 && strlen($p_apellido) <= 25)) {
-				$this->p_apellido = $p_apellido;
-			}
-			else {
-				throw new Exception("Se ingresó un valor no alfabético");
-			}
+			$this->p_apellido = $p_apellido;
 		}
 		
 		public function set_s_apellido($s_apellido) {
-			if (ctype_alpha($s_apellido) and (strlen($s_apellido) <= 25)) {
-				$this->s_apellido = $s_apellido;
-			}
-			else {
-				throw new Exception("Se ingresó un valor no alfabético");
-			}
+			$this->s_apellido = $s_apellido;
 		}
 		
 		public function set_fecha_nacimiento($fecha_nacimiento) {
@@ -251,22 +221,11 @@
 		}
 		
 		public function set_lugar_nacimiento($lugar_nacimiento) {
-			if (ctype_alnum($lugar_nacimiento) and (strlen($lugar_nacimiento) >= 3 && strlen($lugar_nacimiento) <= 300)) {
-				$this->lugar_nacimiento = $lugar_nacimiento;
-			}
-			else {
-				throw new Exception("Se ingresó un valor no alfanumérico");
-			}
+			$this->lugar_nacimiento = $lugar_nacimiento;
 		}
 		
 		public function set_genero($genero) {
-			
-			if (ctype_alpha($genero)) {
-				$this->genero = $genero;
-			}
-			else {
-				
-			}
+			$this->genero = $genero;
 		}
 		
 		public function set_estado_civil($estado_civil) {
@@ -274,15 +233,7 @@
 		}
 		
 		public function set_email($email) {
-
-
-			if (filter_var($email, FILTER_VALIDATE_EMAIL) or empty($email)) {
-				$this->email = $email;
-			} 
-			else {
-				throw new Exception("No es un correo valido");
-			}
-
+			$this->email = $email;
 		}
 		
 		public function set_grado_academico($grado_academico) {
